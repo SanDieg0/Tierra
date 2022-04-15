e16a7d5f089442707a76c946e586e91c8385d6a8
sandiego <rodri4742@hotmail.com>
2022-04-14 22:49:20 -0500

    estrellitas agregadas

--- operaciones.js
+++ operaciones.js
@@ -1,21 +1,26 @@
-// var rootElement = document.documentElement;
-
-var x;
-var y;
-
-// rootElement.style.setProperty("--x", x);
-// rootElement.style.setProperty("--y", y);
-
 function aleatorio(mini, maxi){
     var resultado;
     resultado = Math.floor(Math.random() * (maxi - mini + 1)) + mini;
     return resultado;
 }
 
+var body = document.getElementById("body")
+var x = body.clientWidth;
+var y = body.clientHeight;
 
-document.body.onload = addElement;
-function dibujarEstrella(){
-    const newDiv = document.createElement("div");
-    const currentDiv = document.getElementById("star");
-    document.body.insertBefore(newDiv, planet);
+function estrellitas (){
+    for(i=0; i<aleatorio(500,1000); i++){
+        var estrellitas_container = document.getElementById("estrellitas_container");
+        var star = document.createElement("div");
+        estrellitas_container.appendChild(star);
+        
+        star.style.position = "absolute";
+        star.style.backgroundColor = "#fff";
+        star.style.width = aleatorio(1,1)+"px";
+        star.style.height = aleatorio(1,1)+"px";
+        star.style.borderRadius = "50%";
+        star.style.top = aleatorio(0, 0.99*y)+"px";
+        star.style.left = aleatorio(0, 0.99*x)+"px";
+    }
+
 }
